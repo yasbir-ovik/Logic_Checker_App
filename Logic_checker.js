52884278d74ab93553b0a9fa08644abe9c864d8f
yasbir-ovik <yasbir.634551@gmail.com>
2026-03-13 22:45:26 +0600

    second variable add

--- Logic_checker.js
+++ Logic_checker.js
@@ -1,2 +1,7 @@
 const truthyOrFalsy = true;
 console.log(Boolean(truthyOrFalsy)); // true
+
+const hasDeveloperJob = true;
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
