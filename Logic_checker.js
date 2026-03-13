b21cf3a63e132b38275ba855f3aa8d65a5154adf
yasbir-ovik <yasbir.634551@gmail.com>
2026-03-13 22:50:26 +0600

    false statement add

--- Logic_checker.js
+++ Logic_checker.js
@@ -5,3 +5,9 @@ const hasDeveloperJob = true;
 if (hasDeveloperJob) {
   console.log("Timmy is employed as a developer.");
 }
+
+const isTimmyAGamer = false;
+
+if(isTimmyAGamer){
+  console.log("Timmy loves to play World of Warcraft.")
+}
